1bd19da322835d0b79d37268b122cb1c418abe19
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2023-07-05 01:42:58 +0530

    divided squares into components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component }  from 'react';
-export default function Square() {
+
+
+
+
+function Square({ value }) {
+  return <button className='square'>{value}</button>
+}
+
+
+
+
+export default function Board() {
   return (
     <div>
-      <button className="square">X</button>
-      <button className="square">X</button>
+     
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+      </div>
+      <div className="board-row">
+      <Square value="7"/>
+      <Square value={8}/>
+      <Square value={9}/>
+      </div>
     </div>
   );  
 }
