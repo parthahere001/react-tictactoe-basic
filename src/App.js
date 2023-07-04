7da8d7110b5d00d286b4156d2a33b9c38bd78e4b
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2023-07-05 01:52:53 +0530

    [ADD] click logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component }  from 'react';
+import { useState } from 'react';
 
 
 
-
-function Square({ value }) {
-  return <button className='square'>{value}</button>
+function Square() {
+  const [val, setVal] = useState(null);
+  function handleClick() {
+    setVal('X')
+  }
+  return <button className='square' onClick={handleClick}>{val}</button>
 }
 
 
@@ -15,19 +19,19 @@ export default function Board() {
     <div>
      
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-      <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
+      <Square />
+      <Square />
+      <Square />
       </div>
       <div className="board-row">
-      <Square value="7"/>
-      <Square value={8}/>
-      <Square value={9}/>
+      <Square />
+      <Square />
+      <Square />
       </div>
     </div>
   );  
